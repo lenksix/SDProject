2cff5a7eb2f0d558deafad190db6895ec7ec7e85
chrollo <ilie945@gmail.com>
2019-01-02 18:57:04 +0100

    Added new setup for cassandra

---
+++ project/src/database_servers/InitDb.java
@@ -0,0 +1,40 @@
+package database_servers;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+import com.datastax.driver.core.exceptions.NoHostAvailableException;
+
+/* creates the table we need for querying the urls */
+public class InitDb {
+
+   public static void main(String[] args) {
+      Cluster cluster;
+      Session session;
+      
+      try {
+         cluster = Cluster.builder().addContactPoint("127.0.0.1").build();
+         session = cluster.connect();
+         
+         //Instantiate the keyspace, with standard parameters
+         session.execute("CREATE KEYSPACE IF NOT EXISTS streaming WITH replication = {"
+               + " 'class': 'SimpleStrategy', "
+               + " 'replication_factor': '3' "
+               + "};" );
+         //Connect to the keyspace already instatiated
+         session.execute("USE streaming;");
+         //Create the table 
+         session.execute("CREATE TABLE IF NOT EXISTS channel_vids("
+               + "id_channel uuid,"
+               + "channel_name text,"
+               + "vids map<text,text>, " //where map is <name_video, url>
+               + "PRIMARY KEY(id_channel, channel_name));");  
+      }
+      catch(NoHostAvailableException nhae) {
+         System.out.println("Build failed: <"+ nhae.getMessage() +">");
+         nhae.printStackTrace();
+      }
+      catch(com.datastax.driver.core.exceptions.SyntaxError Dse) {
+         Dse.printStackTrace();
+      }
+   }
+}
